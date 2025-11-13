614246a9d8cee628a746dab17924a69aff1744fe
MoerasGrizzly <moerasgrizzly@protonmail.com>
2025-11-13 08:50:17 +0100

    Update Templates.hpp

--- A3UE/addons/templates/Templates/Templates.hpp
+++ A3UE/addons/templates/Templates/Templates.hpp
@@ -6,7 +6,7 @@ class Templates
         basepath = QPATHTOFOLDER(Templates\3CBF);
         side = "Civ";
         flagTexture = "\A3\Data_F\Flags\Flag_Altis_CO.paa";
-        name = "3CBF";
+        name = "3CBF Vanilla";
         file = "3CBF_Civ";
         description = $STR_A3AP_setupFactionsTab_vanilla_civ;
     }
